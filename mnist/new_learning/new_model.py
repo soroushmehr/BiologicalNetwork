3c49f5b631bae61764048d4fff23820437be4b5b
Ben <benjamin.scellier@gmail.com>
2015-11-30 19:23:19 -0500

    create model based on the 'mutual prediction learning rule'

---
+++ mnist/new_learning/new_model.py
@@ -0,0 +1,239 @@
+import cPickle
+import numpy as np
+import os
+import sys
+import theano
+import theano.tensor as T
+from theano.tensor.shared_randomstreams import RandomStreams
+
+path = os.path.dirname(os.path.abspath(__file__))+os.sep+os.pardir
+sys.path.append(path)
+from outside_world import Outside_World
+from op import MyOp
+
+my_op = MyOp()
+
+class Network(object):
+
+    def __init__(self, path="params.save", batch_size=1, n_hidden=500):
+
+        self.path = path
+        self.batch_size = batch_size
+        self.n_input  = 28*28
+        self.n_hidden = n_hidden
+        self.n_output = 10
+
+        # LOAD/INITIALIZE PARAMETERS
+        self.params = [self.bx, self.W1, self.bh, self.W2, self.by] = self.load_params(path, n_hidden)
+
+        # INITIALIZE STATES
+        self.x = theano.shared(value=np.zeros((self.batch_size, self.n_input),  dtype=theano.config.floatX), name='x', borrow=True)
+        self.h = theano.shared(value=np.zeros((self.batch_size, self.n_hidden), dtype=theano.config.floatX), name='h', borrow=True)
+        self.y = theano.shared(value=np.zeros((self.batch_size, self.n_output), dtype=theano.config.floatX), name='y', borrow=True)
+
+        def rho(s):
+            # return T.clip(s, 0., 1.)       # hard-sigmoid
+            return T.nnet.sigmoid(4.*s-2.) # sigmoid
+            # return T.tanh(s)               # hyperbolic tangent
+
+        def rho_prime(s):
+            # return (s > 0.) * (s < 1.)  # hard-sigmoid
+            r = T.nnet.sigmoid(4.*s-2.) # sigmoid
+            return 4. * r * (1. - r)    # sigmoid
+            # r = T.tanh(s)               # hyperbolic tangent
+            # return 1. - r ** 2          # hyperbolic tangent
+
+        self.rho_x = rho(self.x)
+        self.rho_h = rho(self.h)
+        self.rho_y = rho(self.y)
+
+        self.rho_prime_x = rho_prime(self.x)
+        self.rho_prime_h = rho_prime(self.h)
+        self.rho_prime_y = rho_prime(self.y)
+
+        # LOAD OUTSIDE WORLD
+        self.outside_world = Outside_World(batch_size)
+
+        # CHARACTERISTICS OF THE NETWORK
+        self.prediction = T.argmax(self.y, axis=1)
+
+        def energy_function():
+            squared_norm = ( T.batched_dot(self.x,self.x) + T.batched_dot(self.h,self.h) + T.batched_dot(self.y,self.y) ) / 2.
+            uni_terms    = - T.dot(self.rho_x, self.bx) - T.dot(self.rho_h, self.bh) - T.dot(self.rho_y, self.by)
+            bi_terms     = - T.batched_dot( T.dot(self.rho_x, self.W1), self.rho_h ) - T.batched_dot( T.dot(self.rho_h, self.W2), self.rho_y )
+            return squared_norm + uni_terms + bi_terms
+
+        self.energy = energy_function()
+
+        # THEANO FUNCTIONS
+        rng = np.random.RandomState()
+        self.theano_rng = RandomStreams(rng.randint(2 ** 30)) # used to initialize h and y at random at the beginning of the x-clamped relaxation phase. will also be used when introducing noise in Langevin MCMC
+
+        self.initialize = self.build_initialize_function()
+        self.iterate, self.relax = self.build_iterative_functions()
+
+    def load_params(self, path, n_hidden):
+
+        def initialize_layer(n_in, n_out):
+            rng = np.random.RandomState()
+            W_values = np.asarray(
+                rng.uniform(
+                    low=-np.sqrt(6. / (n_in + n_out)),
+                    high=np.sqrt(6. / (n_in + n_out)),
+                    size=(n_in, n_out)
+                ),
+                dtype=theano.config.floatX
+            )
+            return W_values
+
+        if os.path.isfile(self.path):
+            f = file(path, 'rb')
+            [bx_values, W1_values, bh_values, W2_values, by_values] = cPickle.load(f)
+            f.close()
+        else:
+            bx_values = np.zeros((self.n_input,),  dtype=theano.config.floatX)
+            W1_values = initialize_layer(self.n_input, self.n_hidden)
+            bh_values = np.zeros((self.n_hidden,), dtype=theano.config.floatX)
+            W2_values = initialize_layer(self.n_hidden, self.n_output)
+            by_values = np.zeros((self.n_output,), dtype=theano.config.floatX)
+
+        bx = theano.shared(value=bx_values, name='bx', borrow=True)
+        W1 = theano.shared(value=W1_values, name='W1', borrow=True)
+        bh = theano.shared(value=bh_values, name='bh', borrow=True)
+        W2 = theano.shared(value=W2_values, name='W2', borrow=True)
+        by = theano.shared(value=by_values, name='by', borrow=True)
+
+        return [bx, W1, bh, W2, by]
+
+    def save_params(self):
+        f = file(self.path, 'wb')
+        params = [param.get_value() for param in self.params]
+        cPickle.dump(params, f, protocol=cPickle.HIGHEST_PROTOCOL)
+        f.close()
+
+    def build_initialize_function(self):
+
+        def rho(s):
+            return T.nnet.sigmoid(4.*s-2.)
+
+        x_init = self.outside_world.x_data                                                                           # initialize x=x_data
+        # h_init = T.unbroadcast(T.constant(np.zeros((self.batch_size, self.n_hidden), dtype=theano.config.floatX)),0) # initialize h=0 and y=0
+        # y_init = T.unbroadcast(T.constant(np.zeros((self.batch_size, self.n_output), dtype=theano.config.floatX)),0) # initialize h=0 and y=0
+        # h_init = self.theano_rng.uniform(size=self.h.shape, low=0., high=.01, dtype=theano.config.floatX)            # initialize h at random
+        # y_init = self.theano_rng.uniform(size=self.y.shape, low=0., high=.01, dtype=theano.config.floatX)            # initialize y at random
+        h_init = my_op(2 * (T.dot(rho(x_init), self.W1) + self.bh))                                                  # initialize h by forward propagation
+        y_init = my_op(T.dot(rho(h_init), self.W2) + self.by)                                                        # initialize y by forward propagation
+
+        updates_states = [(self.x, x_init), (self.h, h_init), (self.y, y_init)]
+
+        initialize = theano.function(
+            inputs=[],
+            outputs=[],
+            updates=updates_states
+        )
+
+        return initialize
+
+    def build_iterative_functions(self):
+
+        def states_dot(lambda_x, lambda_y):
+            R_x_bot   = self.outside_world.x_data
+            R_x_top   = self.rho_prime_x * (T.dot(self.rho_h, self.W1.T) + self.bx)
+            x_bot_dot = R_x_bot - self.x
+            x_top_dot = R_x_top - self.x
+            x_dot = lambda_x * x_bot_dot + (1.-lambda_x) * x_top_dot
+
+            R_h_bot = self.rho_prime_h * ( T.dot(self.rho_x, self.W1) + self.bh / 2. )
+            R_h_top = self.rho_prime_h * ( T.dot(self.rho_y, self.W2.T) + self.bh / 2. )
+            h_bot_dot = R_h_bot - self.h
+            h_top_dot = R_h_top - self.h
+
+            R_y_bot = self.rho_prime_y * (T.dot(self.rho_h, self.W2) + self.by)
+            R_y_top = self.outside_world.y_data_one_hot
+            y_bot_dot = R_y_bot - self.y
+            y_top_dot = R_y_top - self.y
+            y_dot = lambda_y * y_top_dot + (1.-lambda_y) * y_bot_dot
+
+            return [x_dot, h_bot_dot, h_top_dot, y_dot]
+
+        def params_dot(Delta_x, Delta_h_bot, Delta_h_top, Delta_y):
+            bx_dot = T.mean(Delta_x, axis=0)
+            W1_dot = T.dot(Delta_x.T, self.rho_h)     / T.cast(self.x.shape[0], dtype=theano.config.floatX)
+            R1_dot = T.dot(self.rho_x.T, Delta_h_top) / T.cast(self.x.shape[0], dtype=theano.config.floatX)
+            bh_dot = T.mean(Delta_h_bot+Delta_h_top, axis=0)
+            W2_dot = T.dot(Delta_h_bot.T, self.rho_y) / T.cast(self.x.shape[0], dtype=theano.config.floatX)
+            R2_dot = T.dot(self.rho_h.T, Delta_y)     / T.cast(self.x.shape[0], dtype=theano.config.floatX)
+            by_dot = T.mean(Delta_y, axis=0)
+
+            return [bx_dot, W1_dot, R1_dot, bh_dot, W2_dot, R2_dot, by_dot]
+
+        lambda_x  = T.fscalar('lambda_x')
+        lambda_h  = T.fscalar('lambda_h')
+        lambda_y  = T.fscalar('lambda_y')
+        epsilon_x = T.fscalar('epsilon_x')
+        epsilon_h = T.fscalar('epsilon_h')
+        epsilon_y = T.fscalar('epsilon_y')
+        alpha_W1  = T.fscalar('alpha_W1')
+        alpha_W2  = T.fscalar('alpha_W2')
+
+        [x_dot, h_bot_dot, h_top_dot, y_dot] = states_dot(lambda_x, lambda_y)
+        h_dot = h_bot_dot + h_top_dot
+
+        Delta_x     = epsilon_x * x_dot
+        Delta_h_bot = epsilon_h * h_bot_dot
+        Delta_h_top = epsilon_h * h_top_dot
+        Delta_y     = epsilon_y * y_dot
+
+        [bx_dot, W1_dot_fwd, W1_dot_bwd, bh_dot, W2_dot_fwd, W2_dot_bwd, by_dot] = params_dot(Delta_x, Delta_h_bot, Delta_h_top, Delta_y)
+
+        Delta_bx     = alpha_W1 * bx_dot
+        Delta_W1_fwd = alpha_W1 * W1_dot_fwd
+        Delta_W1_bwd = alpha_W1 * W1_dot_bwd
+        Delta_W1     = Delta_W1_fwd + Delta_W1_bwd
+        Delta_bh     = alpha_W1 * bh_dot
+        Delta_W2_fwd = alpha_W2 * W2_dot_fwd
+        Delta_W2_bwd = alpha_W2 * W2_dot_bwd
+        Delta_W2     = Delta_W2_fwd + Delta_W2_bwd
+        Delta_by     = alpha_W2 * by_dot
+
+        x_new = self.x + Delta_x
+        h_new = self.h + Delta_h_bot + Delta_h_top
+        y_new = self.y + Delta_y
+
+        bx_new = self.bx + Delta_bx
+        W1_new = self.W1 + Delta_W1
+        bh_new = self.bh + Delta_bh
+        W2_new = self.W2 + Delta_W2
+        by_new = self.by + Delta_by
+
+        # OUTPUTS FOR MONITORING
+        energy_mean  = T.mean(self.energy)
+        error_rate   = T.mean(T.neq(self.prediction, self.outside_world.y_data))
+        mse          = T.mean(((self.y - self.outside_world.y_data_one_hot) ** 2).sum(axis=1))
+        norm_grad_hy = T.sqrt( (h_dot ** 2).mean(axis=0).sum() + (y_dot ** 2).mean(axis=0).sum() )
+        Delta_logW1_fwd = T.sqrt( (Delta_W1_fwd ** 2).mean() ) / T.sqrt( (self.W1 ** 2).mean() )
+        Delta_logW1_bwd = T.sqrt( (Delta_W1_bwd ** 2).mean() ) / T.sqrt( (self.W1 ** 2).mean() )
+        Delta_logW2_fwd = T.sqrt( (Delta_W2_fwd ** 2).mean() ) / T.sqrt( (self.W2 ** 2).mean() )
+        Delta_logW2_bwd = T.sqrt( (Delta_W2_bwd ** 2).mean() ) / T.sqrt( (self.W2 ** 2).mean() )
+
+        # UPDATES
+        updates_params = [(self.bx,bx_new), (self.W1,W1_new), (self.bh,bh_new), (self.W2,W2_new), (self.by,by_new)]
+        updates_states = [(self.x,x_new), (self.h,h_new), (self.y,y_new)]
+
+        # THEANO FUNCTIONS
+        iterative_function = theano.function(
+            inputs=[lambda_x, lambda_y, epsilon_x, epsilon_h, epsilon_y, alpha_W1, alpha_W2],
+            outputs=[energy_mean, norm_grad_hy, self.prediction, error_rate, mse, Delta_logW1_fwd, Delta_logW2_fwd, Delta_logW1_bwd, Delta_logW2_bwd],
+            updates=updates_params+updates_states
+        )
+
+        relaxation_function = theano.function(
+            inputs=[epsilon_h, epsilon_y],
+            outputs=[energy_mean, norm_grad_hy, self.prediction, error_rate, mse],
+            givens={
+            lambda_y: T.constant(0.)
+            },
+            updates=updates_states[1:3]
+        )
+
+        return iterative_function, relaxation_function
---
+++ mnist/new_learning/new_train.py
@@ -0,0 +1,108 @@
+from new_model import *
+
+import numpy as np
+import sys
+from sys import stdout
+import time
+
+if len(sys.argv) > 1:
+    path = sys.argv[1]
+else:
+    path = "params.save"
+batch_size = 20
+n_hidden = 500
+n_epochs = 500
+valid_on = False # validation phase
+
+
+# hyper-parameters for the x-clamped relaxation phase
+n_iterations = 100 # maximum number of iterations
+threshold = .1 # threshold for the norm of grad_hy E to decide when we have reached a fixed point
+
+# hyper-parameters for the learning phase
+eps_h  = np.float32(.5)
+eps_y  = np.float32(.5)
+alpha_W1 = np.float32(.2)
+alpha_W2 = np.float32(.008)
+
+
+
+net = Network(path=path, batch_size=batch_size, n_hidden=n_hidden)
+
+n_batches_train = net.outside_world.train_set_size / batch_size
+n_batches_valid = net.outside_world.valid_set_size / batch_size
+
+print("path = %s, batch_size = %i" % (path, batch_size))
+start_time = time.clock()
+for epoch in range(n_epochs):
+
+    # TRAINING
+    train_energy, train_error, train_cost = 0., 0., 0.
+    relax_iterations, relax_fail = 0., 0.
+    gW1f1, gW2f1, gW1b1, gW2b1 = 0., 0., 0., 0.
+    gW1f2, gW2f2, gW1b2, gW2b2 = 0., 0., 0., 0.
+    # train_energy = energy of the stable configuration (= fixed point) at the end of the x-clamped relaxation phase
+    for index in xrange(n_batches_train):
+        net.outside_world.set(index_new=index, dataset_new=1) # dataset_new=1 means training set
+        net.initialize()
+
+        # X-CLAMPED RELAXATION PHASE
+        for k in range(n_iterations):
+            eps = np.float32(2. / (2.+k)) # common value for eps_h and eps_y
+            [energy, norm_grad_hy, _, error, cost] = net.relax(epsilon_h = eps, epsilon_y = eps)
+            if norm_grad_hy < threshold or k == n_iterations-1:
+                train_energy, train_error, train_cost = train_energy+energy, train_error+error, train_cost+cost
+                relax_iterations, relax_fail = relax_iterations+(k+1.), relax_fail+(k == n_iterations-1)
+                energy_avg = train_energy / (index+1)
+                error_avg = 100. * train_error / (index+1)
+                cost_avg = train_cost / (index+1)
+                iterations_avg = relax_iterations / (index+1)
+                fail_avg = 100. * relax_fail / (index+1)
+                break
+
+        # LEARNING PHASE
+        [_, _, _, _, _, Delta_logW1_fwd_1, Delta_logW2_fwd_1, Delta_logW1_bwd_1, Delta_logW2_bwd_1] = net.iterate(lambda_x = 1., lambda_y = 1., epsilon_x = 0., epsilon_h = eps_h, epsilon_y = eps_y, alpha_W1 = alpha_W1, alpha_W2 = alpha_W2)
+        [_, _, _, _, _, Delta_logW1_fwd_2, Delta_logW2_fwd_2, Delta_logW1_bwd_2, Delta_logW2_bwd_2] = net.iterate(lambda_x = 1., lambda_y = 1., epsilon_x = 0., epsilon_h = eps_h, epsilon_y = eps_y, alpha_W1 = alpha_W1, alpha_W2 = alpha_W2)
+        gW1f1, gW2f1, gW1b1, gW2b1 = gW1f1+Delta_logW1_fwd_1, gW2f1+Delta_logW2_fwd_1, gW1b1+Delta_logW1_bwd_1, gW2b1+Delta_logW2_bwd_1
+        gW1f2, gW2f2, gW1b2, gW2b2 = gW1f2+Delta_logW1_fwd_2, gW2f2+Delta_logW2_fwd_2, gW1b2+Delta_logW1_bwd_2, gW2b2+Delta_logW2_bwd_2
+        stdout.write("\r%2i-train-%3i E=%.1f er=%.2f%% MSE=%.4f it=%.1f fl=%.1f%%" % (epoch, index, energy_avg, error_avg, cost_avg, iterations_avg, fail_avg))
+        stdout.flush()
+
+    stdout.write("\n")
+    dlogW1f1, dlogW2f1, dlogW1f2, dlogW2f2 = 100. * gW1f1 / n_batches_train, 100. * gW2f1 / n_batches_train, 100. * gW1f2 / n_batches_train, 100. * gW2f2 / n_batches_train
+    dlogW1b1, dlogW2b1, dlogW1b2, dlogW2b2 = 100. * gW1b1 / n_batches_train, 100. * gW2b1 / n_batches_train, 100. * gW1b2 / n_batches_train, 100. * gW2b2 / n_batches_train
+    print("   k=1 backward: dlogW1=%.3f%% dlogW2=%.3f%%" % (dlogW1b1, dlogW2b1))
+    print("   k=1 forward:  dlogW1=%.3f%% dlogW2=%.3f%%" % (dlogW1f1, dlogW2f1))
+    print("   k=2 backward: dlogW1=%.3f%% dlogW2=%.3f%%" % (dlogW1b2, dlogW2b2))
+    print("   k=2 forward:  dlogW1=%.3f%% dlogW2=%.3f%%" % (dlogW1f2, dlogW2f2))
+
+    # VALIDATION
+    if valid_on:
+        valid_energy, valid_error, valid_cost = 0., 0., 0.
+        relax_iterations, relax_fail = 0., 0.
+
+        for index in xrange(n_batches_valid):
+            net.outside_world.set(index_new=index, dataset_new=2) # dataset_new=2 means validation set
+            net.initialize()
+
+            # X-CLAMPED RELAXATION PHASE
+            for k in range(n_iterations):
+                eps = np.float32(2. / (2.+k)) # common value for eps_h and eps_y
+                [energy, norm_grad_hy, _, error, cost] = net.relax(epsilon_h = eps, epsilon_y = eps)
+                if norm_grad_hy < threshold or k == n_iterations-1:
+                    valid_energy, valid_error, valid_cost = valid_energy+energy, valid_error+error, valid_cost+cost
+                    relax_iterations, relax_fail = relax_iterations+(k+1.), relax_fail+(k == n_iterations-1)
+                    energy_avg = valid_energy / (index+1)
+                    error_avg = 100. * valid_error / (index+1)
+                    cost_avg = valid_cost / (index+1)
+                    iterations_avg = relax_iterations / (index+1)
+                    fail_avg = 100. * relax_fail / (index+1)
+                    break
+
+            stdout.write("\r   valid-%i  E=%.1f er=%.2f%% MSE=%.4f it=%.1f fl=%.1f%%" % (index, energy_avg, error_avg, cost_avg, iterations_avg, fail_avg))
+            stdout.flush()
+        stdout.write("\n")
+
+    duration = (time.clock() - start_time) / 60.
+    print("   dur=%.1f min" % (duration))
+    net.save_params()
